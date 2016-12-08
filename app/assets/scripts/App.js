3ce0b2e45402e1e021b2e66e107281333ec8479d
Sasha <lyshirokov613@gmail.com>
2016-12-08 23:50:19 +0300

    completed reveal-on-scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import $ from "jquery";
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".skill-item"), "83%");
+new RevealOnScroll($(".team"), "65%");
