212ba10bfa7e61d7d2a63ea010d6c3a5d9826871
Sasha <lyshirokov613@gmail.com>
2016-12-09 19:54:49 +0300

    Completed modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,11 @@ import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import $ from "jquery";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 
 var mobileMenu = new MobileMenu();
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
 new RevealOnScroll($(".skill-item-1"), "91%");
 new RevealOnScroll($(".skill-item-2"), "69%");
 new RevealOnScroll($(".skill-item-3"), "81%");
